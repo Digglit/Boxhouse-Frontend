0e253b3b669ef56ed8705fce4130eaeda8581773
Joseph Marella <Josephmarella97@gmail.com>
2025-01-28 15:19:35 -0500

    Add dynamic blog post links to sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,3 +1,7 @@
+import serverEndpoint from "../utils/serverEndpoint";
+import getBlogSlugs from "../graphql/getBlogSlugs.gql";
+import { print } from "graphql";
+
 export default async function sitemap() {
   // Static URLs
   const myUrls = ["", "/ourWork", "/about", "/blog", "/scheduleConsultation"];
@@ -6,6 +10,27 @@ export default async function sitemap() {
     lastModified: new Date().toISOString(),
   }));
 
+  const response = await fetch(`${serverEndpoint}/graphql`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query: print(getBlogSlugs) }),
+  });
+
+  if (!response.ok) {
+    console.log(await response.json());
+    throw new Error("Failed to fetch slugs for blog posts");
+  }
+
+  const result = await response.json();
+
+  const blogPosts = result.data.blogposts;
+  const blogPostPaths = blogPosts.map((post) => ({
+    url: `https://boxhouseconsulting.com/blog/${post.Slug}`,
+    lastModified: new Date(post.DateWritten).toISOString(),
+  }));
+
   // Combine static and dynamic URLs
-  return [...staticUrls];
+  return [...staticUrls, ...blogPostPaths];
 }
